4c35b6510b8474b940f50f900db05d16354013d2
Andrey Nikonov <mail@einomi.ru>
2016-03-02 23:16:18 +0300

    Add plugin

---
+++ README.md
@@ -0,0 +1,8 @@
+# Typograph
+This is sublime plugin implementation for [Lebedev Typograph](https://www.artlebedev.ru/tools/typograf/)
+
+## Usage
+Select some text and press ctrl+shift+t or call typograph comand from menu (by pressing ctrl+shift+p)
+
+## Contribution
+Plugin is using [RemoteTypograf.py](https://www.artlebedev.ru/tools/typograf/webservice/) script by Art. Lebedev Studio
---
+++ typograph.py
@@ -0,0 +1,15 @@
+import sublime, sublime_plugin
+
+from .RemoteTypograf import RemoteTypograf
+
+class TypographCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		typograph = RemoteTypograf('windows-1251')
+		typograph.htmlEntities()
+		typograph.br(0)
+		typograph.p(0)
+		typograph.nobr(3)
+		for region in self.view.sel():
+			regionText = self.view.substr(region)
+			processed = typograph.processText(regionText)
+			self.view.replace(edit, region, processed)
